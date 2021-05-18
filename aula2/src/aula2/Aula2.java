aa3a39c82dfa95f05dddfeddab357197f93d04b5
Lucas <matheuslucaskid>
2021-05-18 20:36:50 -0300

    comecei um corpo com pequenas funcoes

--- aula2/src/aula2/Aula2.java
+++ aula2/src/aula2/Aula2.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula2;
-
-/**
- *
- * @author Casa
- */
 public class Aula2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+      public static void main(String[] args) {
+       Caneta c1 = new Caneta();
+       c1.cor = "Azul";
+       c1.ponta = 0.5f;
+       c1.tampada = false;
+       
     }
     
 }
---
+++ aula2/src/aula2/Caneta.java
@@ -0,0 +1,18 @@
+package aula2;
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void rabiscar() {
+        
+    }
+    void tampar() {
+        
+    }
+    void destampar() {
+        
+    }
+}
