f7e216910aca09a71dc1bd984c728b730dd13f0e
Lucas <matheuslucaskid>
2021-05-20 18:27:51 -0300

    defini toda a classe c1 da caneta

--- aula2/src/aula2/Aula2.java
+++ aula2/src/aula2/Aula2.java
@@ -4,8 +4,10 @@ public class Aula2 {
        Caneta c1 = new Caneta();
        c1.cor = "Azul";
        c1.ponta = 0.5f;
-       c1.tampada = false;
-       
+       c1.tampar();
+       c1.status();
+       c1.rabiscar();
+      
     }
     
 }
--- aula2/src/aula2/Caneta.java
+++ aula2/src/aula2/Caneta.java
@@ -5,14 +5,26 @@ public class Caneta {
     float ponta;
     int carga;
     boolean tampada;
-    
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? "  + this.tampada);
+    }
+      
     void rabiscar() {
-        
-    }
+        if (this.tampada == true) {
+            System.out.println("ERRO! não posso rabiscar.");
+    } else {
+            System.out.println("Estou rabiscando.");
+    
+      }
+}
     void tampar() {
-        
+       this.tampada = true
     }
-    void destampar() {
-        
+    void destampar() {  
+        this.tampada = false
     }
 }
