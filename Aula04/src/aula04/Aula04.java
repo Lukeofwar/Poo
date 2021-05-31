84053a448a4d3b9783e1ff3560ea0e192557e0e6
Lukeofwar <matheuslucaskid@gmail.com>
2021-05-31 18:22:33 -0300

    iniciei uma aula e fiz outros exercícios

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,12 @@
+package aula04;
+  public class Aula04 {
+    public static void main(String[] args) {
+        
+       Caneta c1 = new Caneta("Bic", "vermelho", 0.8f );        
+            c1.status();
+                          
+            Caneta c2 = new Caneta("kkk", "verde", 0.4f);
+            c2.status();
+    }
+
+}
---
+++ Aula04/src/aula04/Caneta.java
@@ -0,0 +1,50 @@
+package aula04;
+
+public class Caneta {
+
+    private String modelo;
+    private float ponta;
+    private String cor;
+    private boolean tampada;
+    
+    public Caneta(String m, String c, float p) { //Este é o método construtor
+             this.modelo = m;
+             this.cor = c;
+             this.ponta = p;
+             this.tampar();        
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String m) {
+        this.modelo = m;
+    }
+
+    public float getPonta() {
+        return this.ponta;
+    }
+
+    public void setPonta(float p) {
+        this.ponta = p;
+
+    }
+    
+    public void tampar() {
+             this.tampada = true; 
+    }
+
+    public void destampar() {
+        this.tampada = false;
+    }
+    public void status() {
+        System.out.println("SOBRE A CANETA:");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " + this.tampada);
+    }
+}
+
+    
