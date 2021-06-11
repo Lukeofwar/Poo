470a4dd153d6420bcaeaf87176107b7d12990cb0
Lukeofwar <matheuslucaskid@gmail.com>
2021-06-10 21:14:58 -0300

    tendo uma melhor visualização de poo e recriando suas funções

--- aula2/src/aula2/Aula2.java
+++ aula2/src/aula2/Aula2.java
@@ -2,21 +2,20 @@ package aula2;
 public class Aula2 {
       public static void main(String[] args) {
        Caneta c1 = new Caneta();
-       c1.cor = "Azul";
-       c1.modelo ="Injex Pen";
-       c1.ponta = 0.5f;
-       c1.tampar();
-       c1.status();
-       c1.rabiscar();
        
-       Caneta c2 = new Caneta();
-       c2.modelo = "Faber Catell";
-       c2.cor = "preta";
-       c2.ponta = 2.5f;
-       c2.rabiscar();
-       c2.status();
-       c2.destampar();
-      
-    }
-    
+        c1.cor = "Azul cristal";
+        c1.ponta = 0.6f;
+        c1.tampar();
+        c1.modelo = "Bic cristal";
+        c1.status();
+        c1.rabiscar()  ;
+        
+        Caneta c2 = new Caneta();
+        c2.modelo = "kkkk";
+        c2.cor = "preta";
+        c2.destampar();   
+        c2.status();
+        c2.rabiscar();
+        
+        }   
 }
--- aula2/src/aula2/Caneta.java
+++ aula2/src/aula2/Caneta.java
@@ -1,32 +1,34 @@
 package aula2;
 public class Caneta {
+        
     String modelo;
     String cor;
     float ponta;
     int carga;
     boolean tampada;
+    
     void status() {
-        System.out.println("Carga: " + this.carga + "50%" );
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Uma caneta: " + this.cor);
+        System.out.println("Uma Caneta: " + this.cor + "\n");
         System.out.println("Ponta: " + this.ponta);
-        System.out.println("Está tampada? "  + this.tampada);
-    }
-      
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+}
+    
     void rabiscar() {
         if (this.tampada == true) {
-            System.out.println("ERRO! não posso rabiscar." + "\n");
-    } else {
-            System.out.println("Estou rabiscando.");
+            System.out.println("ERRO! não posso rabiscar.");
+        } else {
+             System.out.println("Estou  Rabiscando.");       
+        }
+    }
     
-      }
-}
     void tampar() {
-       this.tampada = true;
-    }
-    void destampar() {  
-        this.tampada = false;
+        this.tampada = true;
     }
     
+    void destampar() {
+      this.tampada = false;
+    }
 }
 
