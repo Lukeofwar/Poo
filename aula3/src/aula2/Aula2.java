65a1971042874b9e43831084a08a34bcc6b36410
Lukeofwar <matheuslucaskid@gmail.com>
2021-06-11 21:09:27 -0300

    refazendo o curso do 0 para entender melhor sobre poo

--- aula3/src/aula2/Aula2.java
+++ aula3/src/aula2/Aula2.java
@@ -4,15 +4,15 @@ public class Aula2 {
 
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
-
-        c1.modelo = "injex pen";
-        c1.cor = "verde";
-        // c1.ponta = 0.51f;
-        c1.carga = 80;
-        // c1.tampada = false;
-        c1.destampar();
+        //c1.ponta = 0.5f;0
+        c1.carga = 75;
+        //c1.tampada = false;
+        c1.tampar();
+        c1.modelo = "layssa";
+        c1.cor = "vermelha";      
         c1.status();
         c1.rabiscar();
+        
 
     }
 }
