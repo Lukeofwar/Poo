310e4419ec1e017d512927cf2137a91dfcee7a55
Lukeofwar <matheuslucaskid@gmail.com>
2021-06-17 20:44:02 -0300

    criei outra aula e fiz aprendi melhor getters e setters

--- Aula04/src/aula04/Aula04.java
+++ Aula04/src/aula04/Aula04.java
@@ -1,12 +1,10 @@
 package aula04;
   public class Aula04 {
     public static void main(String[] args) {
-        
-       Caneta c1 = new Caneta("Bic", "vermelho", 0.8f );        
-            c1.status();
-                          
-            Caneta c2 = new Caneta("kkk", "verde", 0.4f);
-            c2.status();
+         Caneta c1 = new Caneta("Lay", "amarelo", 0.4f);
+         c1.status();
+         
+         Caneta c2 = new Caneta("love lay", "vermelho", 0.6f);
+         c2.status();
     }
-
-}
+  }
--- Aula04/src/aula04/Caneta.java
+++ Aula04/src/aula04/Caneta.java
@@ -4,47 +4,41 @@ public class Caneta {
 
     private String modelo;
     private float ponta;
-    private final String cor;
+    private String cor;
     private boolean tampada;
     
-    public Caneta(String m, String c, float p) { //Este é o método construtor
-             this.modelo = m;
-             this.cor = c;
-             this.ponta = p;
-             this.tampar();       
-    }
-
-    public String getModelo() {
-        return this.modelo;
-    }
-
-    public void setModelo(String m) {
+    public Caneta(String m, String c, float p) { //este é o método Construtor
         this.modelo = m;
-    }
-
-    public float getPonta() {
-        return this.ponta;
-    }
-
-    public void setPonta(float p) {
+        this.cor = c;
         this.ponta = p;
-
+        this.tampar();
     }
     
-    public final void tampar() {
-             this.tampada = true; 
-    }
-
-    public void destampar() {
-        this.tampada = false;
-    }
-    public void status() {
-        System.out.println("SOBRE A CANETA:" + "\n"  );
-        System.out.println("Modelo: " + this.getModelo());
-        System.out.println("Ponta: " + this.getPonta());
-        System.out.println("Cor: " + this.cor);
-        System.out.println("Tampada: " + this.tampada);
-    }
+      public String getModelo() {
+          return this.modelo;
+     }  
+      public void setModelo(String m) {
+          this.modelo = m;
+      }
+      public float getPonta() {
+          return this.ponta; 
+   }
+      public void setPonta(float p) {
+          this.ponta = p;
+      }
+      public void tampar() {
+          this.tampada = true;
+      }
+      public void destampar() {
+          this.tampada = false;
+      }
+      public void status() {
+          System.out.println("Sobre A Caneta:" + "\n");
+          System.out.println("Modelo: " + this.getModelo());
+          System.out.println("Ponta: " + this.getPonta());
+          System.out.println("Cor: " + this.cor);
+          System.out.println("Tampada: " + this.tampada);
+      }
 }
 
     
